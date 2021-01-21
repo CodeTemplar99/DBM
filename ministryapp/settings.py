10b47b5a72efffc336f3c2cdcbe03b843ce42137
Codetemplar99 <udoagwac@gmail.com>
2021-01-21 06:47:08 +0100

    static across

--- ministryapp/settings.py
+++ ministryapp/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # 'ministryapp.apps.MinistryappConfig',
     'aboutministry.apps.AboutministryConfig',
     'days.apps.DaysConfig',
     'contact.apps.ContactConfig',
