66baeb036d0a0007a15a5613b98fa24f92d1ec26
Codetemplar99 <udoagwac@gmail.com>
2021-03-22 13:43:36 +0100

    tenary for display

--- announcements/models.py
+++ announcements/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Announcements(models.Model):
+    id = models.AutoField(primary_key=True)
+    cc = models.CharField(blank=False, null=False, max_length=120)
+    announcements = models.TextField(max_length=200, blank=False, null=False)
+    date_created = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.Announcements
--- events/static/js/main.js
+++ events/static/js/main.js
@@ -3,16 +3,31 @@ const eventBox = document.querySelector('#event-box')
 
 const eventDate = Date.parse(eventBox.textContent)
 
-setInterval(() => {
+const interval = setInterval(() => {
   const now = new Date().getTime()
 
+  if (eventDate <= now)
+  return clearInterval(interval) 
   const d = Math.floor(eventDate / (1000 * 60 * 60 * 24) - (now / (1000 * 60 * 60  * 24)))
   const h = Math.floor((eventDate / (1000 * 60 * 60) - (now / (1000 * 60 * 60 ))) % 24)
   const m = Math.floor((eventDate / (1000 * 60 ) - (now / (1000 * 60 ))) % 60)
   const s = Math.floor((eventDate / (1000) - (now / (1000))) % 1000)
-  document.querySelector('#d-left-space').innerHTML= d
-  document.querySelector('#h-left-space').innerHTML= h
-  document.querySelector('#m-left-space').innerHTML= m
-  document.querySelector('#s-left-space').innerHTML= s
+  document.querySelector('#d-left-space').innerHTML= d < 0 ? 0 : d
+  document.querySelector('#h-left-space').innerHTML= h < 0 ? 0 : h
+  document.querySelector('#m-left-space').innerHTML = m < 0 ? 0 : m
+  document.querySelector('#s-left-space').innerHTML = s < 0 ? 0 : s
   
 }, 1000);
+
+var i = 0;
+var txt = ' Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime, rem?'; /* The text */
+var speed = 60; /* The speed/duration of the effect in milliseconds */
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.querySelector('#home-text').innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+ 
