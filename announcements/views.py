886e37e710835aefc09aa20734c9b247f476eb8a
Codetemplar99 <udoagwac@gmail.com>
2021-05-07 10:12:47 +0100

    announcement page

--- announcements/views.py
+++ announcements/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404
+from .models import Announcement
 # Create your views here.
 
 
 def announcements(request):
-    return render(request, 'announcements.html')
+    results = Announcement.objects.all()
+    context = {'results': results, }
+    return render(request, 'announcements.html', context)
